2e5d8d690c4560f922ebf964c891b742a0d13d83
Justin Teng <nekasrofd@Justins-MacBook-Pro.local>
2019-09-23 00:43:08 -0700

    made changes for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
+const path = require('path');
 const cors = require('cors');
 
 // Load env
@@ -19,6 +20,16 @@ app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/foods', require('./routes/foods'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const port = process.env.PORT || 8000;
 
