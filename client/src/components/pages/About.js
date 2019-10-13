53e270565c07c00cf5e36fff2fd47af1e504d37f
Justin Teng <nekasrofd@justins-mbp.lan>
2019-10-12 23:54:06 -0700

    added new icon

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -7,6 +7,9 @@ const About = () => {
       <p>App to determine whether or not a food is safe for a dog to eat</p>
       <p>Version: 1.0.0</p>
       <p>Dedicated to Emmy</p>
+      <p>
+        Created by <a href='https://justinteng.com'>Justin Teng</a>
+      </p>
     </Fragment>
   );
 };
