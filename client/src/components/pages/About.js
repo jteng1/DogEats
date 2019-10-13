d84d308d03272a6633025c6086cfbce6546f6f2e
Justin Teng <nekasrofd@justins-mbp.lan>
2019-10-12 23:39:22 -0700

    testing another icon

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>App to determine whether or not a food is safe for a dog to eat</p>
-      <p>Version: 1.1.0</p>
+      <p>Version: 1.0.0</p>
       <p>Dedicated to Emmy</p>
     </Fragment>
   );
